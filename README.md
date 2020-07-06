dfa102a25314485e7d00f51751455c161102a037
yongrae <kyr9389@naver.com>
2020-07-06 23:12:59 +0900

    #13 Smart vs Dumb Components
    Signed-off-by: yongrae <kyr9389@naver.com>

--- README.md
+++ README.md
@@ -18,7 +18,7 @@ React 학습의 목적으로 [링크](https://www.youtube.com/playlist?list=PL7j
 10. Thinking in React Component State ✔
 11. Practicing this setState ✔
 12. Loading states ✔
-13. Smart vs Dumb Components ❌
+13. Smart vs Dumb Components ✔
 14. AJAX on React ❌
 15. Promises ❌
 16. Async Await ❌
--- src/Movie.js
+++ src/Movie.js
@@ -2,36 +2,28 @@ import React, { Component } from "react";
 import PropTypes from 'prop-types';
 import './Movie.css';
 
-class Movie extends Component {
-
-    static propTypes = {
-        title: PropTypes.string.isRequired,
-        poster: PropTypes.string.isRequired
-    }
-
-    render(){
-        return(
-            <div>
-                <MoviePoster poster={this.props.poster}/>
-                <h1>{this.props.title}</h1>
-            </div>
-        );
-    }
+function Movie({title, poster}) {
+    return (
+        <div>
+            <MoviePoster poster={poster}/>
+            <h1>{title}</h1>
+        </div>
+    )
 }
 
+function MoviePoster({poster}) {
+    return (
+        <img src={poster} alt="Movie Poster"/>
+        )
+}
 
+Movie.propTypes = {
+    title: PropTypes.string.isRequired,
+    poster: PropTypes.string.isRequired
+}
 
-class MoviePoster extends Component {
-
-    static propTypes = {
-        poster: PropTypes.string.isRequired
-    }
-
-    render() {
-        return (
-            <img src={this.props.poster}/>
-        );
-    }
+MoviePoster.propTypes = {
+    poster: PropTypes.string.isRequired
 }
 
 export default Movie;
