1a47ac2226e8a78edca288ba69419e6c7efd0201
yongrae <kyr9389@naver.com>
2020-07-09 01:44:31 +0900

    #17 Updating Movie : Component
    Signed-off-by: yongrae <kyr9389@naver.com>

--- README.md
+++ README.md
@@ -22,7 +22,7 @@ React 학습의 목적으로 [링크](https://www.youtube.com/playlist?list=PL7j
 14. AJAX on React ✔
 15. Promises ✔
 16. Async Await ✔
-17. Updating Movie : Component ❌
+17. Updating Movie : Component ✔
 18. CSS for Moive ❌
 19. Building for Production ❌
 20. The END! ❌
--- src/App.js
+++ src/App.js
@@ -14,7 +14,13 @@ class App extends Component {
 
     _renderMovies = () => {
         const movies = this.state.movies.map(movie => {
-            return <Movie title={movie.title} poster={movie.large_cover_image} key={movie.id} />
+            console.log(movie);
+            return <Movie title={movie.title_english}
+                          poster={movie.medium_cover_image}
+                          key={movie.id}
+                          genres={movie.genres}
+                          synopsis={movie.synopsis}
+            />
         })
 
         return movies
